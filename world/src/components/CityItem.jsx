6cbec60028e355cee62c8c1090de6c9927872374
JoanHc <campero123joan@gmail.com>
2026-04-13 22:54:14 -0600

    Subiendo estructura completa del proyecto

--- world/src/components/CityItem.jsx
+++ world/src/components/CityItem.jsx
@@ -1,12 +1,12 @@
-import { useCities } from '../contexts/CitiesContext';
-import styles from './CityItem.module.css';
-import { Link } from 'react-router-dom';
+import { useCities } from "../contexts/CitiesContext";
+import styles from "./CityItem.module.css";
+import { Link } from "react-router-dom";
 
 const formatDate = (date) =>
-  new Intl.DateTimeFormat('en', {
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric',
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
   }).format(new Date(date));
 
 function CityItem({ city }) {
@@ -22,17 +22,14 @@ function CityItem({ city }) {
     <li>
       <Link
         className={`${styles.cityItem} ${
-          (id === currentCity, id ? styles['cityItem--active'] : '')
+          (id === currentCity, id ? styles["cityItem--active"] : "")
         }`}
-        to={`${id}?lat=${position.lat}&lng=${position.lng}`}
+        to={`${id}?lat=${position.position__lat}&lng=${position.position__lng}`}
       >
         <span className={styles.emoji}>{emoji}</span>
         <h3 className={styles.name}>{cityName}</h3>
         <time className={styles.date}>({formatDate(date)})</time>
-        <button
-          className={styles.deleteBtn}
-          onClick={handleClick}
-        >
+        <button className={styles.deleteBtn} onClick={handleClick}>
           &times;
         </button>
       </Link>
